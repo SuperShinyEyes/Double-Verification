5b5675e76cf4fc9d5866b31bd1ed0ed3de5da92b
Young Park <i.make.seyoung@gmail.com>
2015-08-30 00:15:38 +0900

    Made pyfacescmd as a library

--- pyfacescmd/pyfacesdemo/__main__.py
+++ pyfacescmd/pyfacesdemo/__main__.py
@@ -34,7 +34,7 @@ def crop_img(img):
   print img2.size
 
 CWP = os.getcwd()   # Get current working directory
-# CWP = '/'.join(CWP.split('/')[:-1])
+CWP_parent = '/'.join(CWP.split('/')[:-1])
 
 def run_bash_cmd(cmd):
   process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
@@ -54,7 +54,7 @@ imgname = CWP + "/image_cropped.jpg"
 print("Run face recognition!")
 # FACE_REC_COMMAND = "python pyfacescmd/pyfacesdemo %s 12 3" % image_path
 
-dirname = CWP + "/images/gallery/"
+dirname = CWP_parent + "/images/gallery/"
 egfaces = 12
 thrshld = 3
 pyf=pyfaces.PyFaces(imgname,dirname,egfaces,thrshld)
